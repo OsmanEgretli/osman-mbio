2a333b5007989e672b9f7d41785adad96a2e6192
OsmanEgretli <oegretli@gmail.com>
2023-08-24 09:27:56 +0200

    mochareport fix

--- cypress/support/actions/CarConfiguratorPageActions.js
+++ cypress/support/actions/CarConfiguratorPageActions.js
@@ -7,6 +7,7 @@ class CarConfiguratorPageActions {
 
     selectAFuelType(fuelType) {
 
+<<<<<<< HEAD
         carConfigurator.getOpenFuelTypeDropdown()
             .scrollIntoView({block: 'center', inline: 'center'}) // This centers the element
             .click({force: true});
@@ -37,4 +38,34 @@ class CarConfiguratorPageActions {
     }
 }
 
+
+    carConfigurator.getOpenFuelTypeDropdown()
+    .scrollIntoView({ block: 'center', inline: 'center' }) // This centers the element
+    .click({force: true});
+    carConfigurator.getOpenFuelTypeItem(fuelType).click({force: true})
+    cy.wait(500)
+    carConfigurator.getOpenFuelTypeDropdown().click({force: true})
+    carConfigurator.getOpenFuelTypeDropdown().click({force: true})
+}
+
+    saveHighestAndLowestPrices() {
+        // Using the function to get all car prices
+     carConfigurator.getCarPriceFromCard().invoke('text').then((text) => {
+            // Splitting prices based on spaces
+            const pricesText = text.split(/\s+/).filter(t => t.startsWith('£'));
+            cy.log('Prices Text:', pricesText);
+            const prices = pricesText.map(price => {
+                const parsedPrice = parseFloat(price.trim().replace('£', '').replace(',', ''));
+                cy.log(`Converted ${price} to ${parsedPrice}`); // Log each conversion
+                return parsedPrice;
+            });
+            const highestPrice = Math.max(...prices);
+            const lowestPrice = Math.min(...prices);
+            cy.log('Highest Price:', highestPrice);
+            cy.log('Lowest Price:', lowestPrice);
+            const content = `Highest Price: £${highestPrice.toLocaleString()}\nLowest Price: £${lowestPrice.toLocaleString()}`;
+            cy.writeFile('car_prices.txt', content);
+        });
+    }
+}
 export default CarConfiguratorPageActions
